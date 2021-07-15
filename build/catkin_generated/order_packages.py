6fe91e12587ff0747b648e9e30d4961acff345e9
SLINIS <nilsmailiseke@gmail.com>
2021-07-15 11:02:18 +0200

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/nils/Documents/Studium/Studienarbeit/DossArm/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/nils/Documents/Studium/Studienarbeit/DossArm/devel;/home/nils/moveo/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/nils/Documents/Studium/Studienarbeit/DossArm/devel;/home/nils/moveo/catkin_ws/devel;/opt/ros/noetic' != '' else []
